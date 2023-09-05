324ecadf7658d83c98dda92e0cb75a38b250cd7c
Jessica Serqueira <jessica.serqueira7@gmail.com>
2023-09-05 14:19:27 -0300

    feat: Configuração do layout

--- DevPoliChallenge/DevPoliChallenge/Features/MainView/MainViewController.swift
+++ DevPoliChallenge/DevPoliChallenge/Features/MainView/MainViewController.swift
@@ -10,10 +10,22 @@ import UIKit
 
 class MainViewController: UIViewController {
     var customView = MainView()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view = customView
+        customView.delegate = self
         customView.backgroundColor = .white
+    }
+}
+
+extension MainViewController: MainViewDelegate {
+    func showMyProfile() {
+    
     }
 }
