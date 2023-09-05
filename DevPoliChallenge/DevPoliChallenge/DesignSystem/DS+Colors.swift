9ad20518e29b2902a7e480e7286735dd9581718a
Jessica Serqueira <jessica.serqueira7@gmail.com>
2023-09-05 09:44:03 -0300

    feat: subindo projeto para o repositório git

---
+++ DevPoliChallenge/DevPoliChallenge/DesignSystem/DS+Colors.swift
@@ -0,0 +1,20 @@
+//
+//  DS+Colors.swift
+//  DevPoliChallenge
+//
+//  Created by DevPoli on 29/07/23.
+//  Copyright © 2023 DevPoli. All rights reserved.
+//
+
+import UIKit
+
+extension DesignSystem {
+
+    enum Colors {
+        static let primary: UIColor = .init(hex: "#A24FF6")
+        static let secondary: UIColor = .init(hex: "#7343A3")
+        static let tertiary: UIColor = .init(hex: "#3F3F3F")
+        static let accent: UIColor = .init(hex: "#E39438")
+        static let background: UIColor = .init(hex: "#F0F2FE")
+    }
+}
